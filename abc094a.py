3b30232272f96b14999349218818a12882a5869d
euq <choco.87.nano@gmail.com>
2018-04-17 10:44:40 +0900

    initial commit

--- abc094a.py
+++ abc094a.py
@@ -1,6 +1,7 @@
 #a, b, x = map(lambda anm: int(anm),input().split(' '))
 #lambda: 無名関数
 a, b, x = map(int,input().split(' '))
+#int関数を引数なしで使っているのはのはmapに渡しているからできること
 if a <= x <= a + b:
   print('YES')
 else:
