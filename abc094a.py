9960994a16b6d8133152566e423e4d29a546fed2
luna <38135217+euq@users.noreply.github.com>
2018-04-20 11:19:27 +0900

    Delete abc094a.py

--- abc094a.py
+++
@@ -1,9 +0,0 @@
-#a, b, x = map(lambda anm: int(anm),input().split(' '))
-#lambda: 無名関数
-a, b, x = map(int,input().split(' '))
-#int関数はもともと引数なしでも使えるのかmapに渡してるから引数なしなのかどっち？
-if a <= x <= a + b:
-  print('YES')
-else:
-  print('NO')
-
