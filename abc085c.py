84ff83807ff2d41b27c6ffb8435956c8da372782
Haruna <choco.87.nano@gmail.com>
2018-05-16 17:53:49 +0900

    add file

---
+++ abc085c.py
@@ -0,0 +1,18 @@
+f int(y/1000) <= n:
+  man = 0
+    gos = 0
+      sen = int(y/1000)
+      elif ((int(y/5000)) <= n) & ((int((y%5000)/1000)) <= n):
+        man = 0
+	  gos = int(y/5000)
+	    sen = int((y%5000)/1000)
+	    elif (int(y/10000)) <= n & ((int((%5000)/1000)) <= n)
+	      
+	        if int(man)%5000 == 0:
+		      gos = int(man/5000)
+		          sen = 0
+			    else:
+			          gos = int(man/5000)
+				      sen = int(gos/1000)
+
+				      print(man, gos, sen)
